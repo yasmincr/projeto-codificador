0a2ed3edb0ce35610b9a843c3b62e9e375b9ce11
Yasmin <112868025+yasmincr@users.noreply.github.com>
2022-10-04 16:42:11 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,65 @@
+
+var escolhas = document.querySelector("select");
+
+escolhas.addEventListener("change", function (evento) {
+  evento.preventDefault();
+
+  var incremento = document.getElementById("valorIncremento");
+
+  if (evento.target.value == "cCesar") {
+    incremento.style = "display: flex";
+  } else {
+    incremento.style = "display: none";
+  }
+});
+
+
+var formulario = document.forms.formulario;
+
+formulario.addEventListener("submit", function (evento) {
+  evento.preventDefault();
+
+  var texto = formulario.texto.value;
+  var escolha = formulario.escolherCod.value;
+  var botoes = formulario.codific.value;
+  var numeroIncremento = formulario.numeroIncrementos.value;
+  var mensagemFinal = "";
+
+  if (escolha == "cCesar") {
+    mensagemFinal = cesar(botoes, texto, numeroIncremento);
+  } else {
+    mensagemFinal = base64(botoes, texto);
+  }
+
+  var resultadoTexto = document.getElementById("saidaTexto");
+  resultadoTexto.innerHTML = `${mensagemFinal}`;
+});
+
+
+function cesar(codific, texto, numeroIncremento) {
+  numeroIncremento = Number(numeroIncremento);
+
+  var mensagemFinal = "";
+
+  for (var i = 0; i < texto.length; i++) {
+    var letra = texto[i];
+    var codigo = letra.charCodeAt();
+
+    if (codific == "codificar") {
+      codigo += numeroIncremento;
+    } else {
+      codigo -= numeroIncremento;
+    }
+    mensagemFinal += String.fromCharCode(codigo);
+  }
+  return mensagemFinal;
+}
+
+
+function base64(codific, texto) {
+  if (codific == "codificar") {
+    return btoa(texto);
+  } else {
+    return atob(texto);
+  }
+}
